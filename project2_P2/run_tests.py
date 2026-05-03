ff4ef6d7b2de102516568f0471aed3942312cd54
zosianarloch <zosia_narloch@wp.pl>
2026-05-03 15:23:11 +0200

    Add JSHOP2, new domain, problems for exercise 1.2

---
+++ project2_P2/run_tests.py
@@ -0,0 +1,49 @@
+import subprocess
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+
+runtime = []
+num = [3]
+
+open("outputs.txt", "w").close()
+
+while True:
+    try:
+        program = subprocess.run(["python3","./generate_problems.py", "-d", "1", "-r", "0" ,"-l" ,f"{num[-1]}" ,"-p", f"{num[-1]}" ,"-c" ,f"{num[-1]}" ,"-g", f"{num[-1]}"], capture_output=True, text=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with return code {e.returncode}")
+        
+    subprocess.run("rm -f *.java *.class p2/*.java p2/*.class", shell=True)
+    
+    start = time.perf_counter()
+    try:
+        result = subprocess.run(["./jshop2-console.sh", "domains/p2/p2", "domains/p2/problem"], capture_output=True, text=True)
+        print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with return code {e.returncode}")
+    
+    end = time.perf_counter()
+    
+    with open("outputs.txt", "a") as f:
+        f.write(result.stdout)
+
+    new_num = num[-1] + 25
+    new_runtime = end - start
+    runtime.append(new_runtime)
+    
+    if new_num < 400:
+        num.append(new_num)
+        print(new_runtime)
+    else: 
+        break
+    
+print(num)
+print(runtime)
+
+plt.plot(num, runtime, marker='o')
+plt.xlabel('Size')
+plt.ylabel('Time[s]')
+plt.savefig('Figure_1.png')
+plt.show()
